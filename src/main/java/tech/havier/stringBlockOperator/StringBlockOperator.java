e23025ae10a0dc777ce52733a25305d223a428ec
HavierDai <huaizhi.dai@nre.tas.gov.au>
2022-10-18 09:14:45 +1100

    version 0.13 developing. adding new version of StringBlockOperator.

--- src/main/java/tech/havier/stringBlockOperator/StringBlockOperator.java
+++ src/main/java/tech/havier/stringBlockOperator/StringBlockOperator.java
@@ -1,29 +1,25 @@
 package tech.havier.stringBlockOperator;
 
 
-import tech.havier.Dictionaries;
-import tech.havier.PerformanceMonitor;
-import tech.havier.databaseOperator.DatabaseFactory;
-import tech.havier.databaseOperator.DatabaseOperator;
-import tech.havier.stringBlockDelegate.StringBlockImporter;
-import tech.havier.timeToolkit.HavierTimer;
 
 import java.sql.SQLException;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public interface StringBlockOperator {
 
     int savedWordsNumber();
     int needCheckWordsNumber();
     int unknownWordsNumber();
+
     void printSavedWords();
     void printConvertedWords();
     void printUnknownWords();
-    void uploadWords() throws SQLException, ClassNotFoundException ;
-    void bunchIgnore(Scanner scanner, int[] indexes);
+
+    void uploadWords() throws SQLException, ClassNotFoundException;
     void uploadCurrentAasBDictionary();
+
+    void bunchIgnore(Scanner scanner, int[] indexes);
+
     void checkConvertedWords(Scanner scanner, String[] correctingWords);
     void checkUnknownWords(Scanner scanner) throws SQLException, ClassNotFoundException;
 
